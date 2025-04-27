35e2364b0abc52fdac4dfc10bafc8f2784b366cc
Vishal Raj <vishal@binder-sa.com>
2025-04-27 13:29:42 +0530

    git commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://localhost:8000',
+  // BASE_URL: 'http://localhost:8000',
+  BASE_URL: 'https://productivity-app-backend-comp.onrender.com',
 };
