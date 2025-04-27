05f12eb65a2fd898a61314ceaf617bf1c80ecc7b
Vishal Raj <vishal@binder-sa.com>
2025-04-27 14:32:49 +0530

    changes

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -4,18 +4,14 @@ import { USER_ROLES_DESC } from 'src/app/enums/user-role.enum';
 import { FilesService } from 'src/app/services/files.service';
 import { SharedModule } from 'src/app/shared.module';
 import { CountdownTimerComponent } from '../countdown-timer/countdown-timer.component';
-import {
-  BANNER_TYPE_DESC_ENUM,
-  BANNER_TYPE_ID_ENUM,
-} from 'src/app/enums/banner-type.enum';
-import { SelectedFileComponent } from '../selected-file/selected-file.component';
+import { BANNER_TYPE_ID_ENUM } from 'src/app/enums/banner-type.enum';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
   standalone: true,
-  imports: [SharedModule, CountdownTimerComponent, SelectedFileComponent],
+  imports: [SharedModule, CountdownTimerComponent],
 })
 export class BannerComponent implements OnInit {
   @Input() bannerDetails: any;
